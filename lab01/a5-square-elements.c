bb4327a763cf4a22953da4dfbf5a86dadc98afab
Duda Ferreira <ferreira.mev@gmail.com>
2021-11-19 09:38:33 -0300

    Write function called by each thread

--- lab01/a5-square-elements.c
+++ lab01/a5-square-elements.c
@@ -10,3 +10,47 @@ Programa que eleva ao quadrado os elementos de um vetor, alterando o vetor inici
 #include <pthread.h>
 
 #define NTHREADS 2  // número de threads (além do main)
+#define NELEMS 10000  // número de entradas do vetor
+
+#define DEBUG
+
+typedef struct
+{
+    int thr_n;  // número da thread
+    int* vector;  // o array, em sua totalidade
+    // (ponteiro p/ sua posição inicial)
+} thr_args;
+
+void* square_elems(void* arg)
+/* Recebe um ponteiro para uma struct do tipo thr_args e eleva ao quadrado parte das entradas do vetor apontado por seu campo vector,
+de acordo com o número da thread thr_n. */
+{
+    thr_args* args = (thr_args*) arg;
+
+    // Quantidade de elementos pelos quais a thread é responsável:
+    int nelems = (NELEMS / NTHREADS) - 1;
+
+    if ((NELEMS % NTHREADS) > args->thr_n)
+    {
+        nelems++;
+    }
+
+    for (int j=0; j < nelems; j++)
+    {
+        int pos = j * NTHREADS + args->thr_n;
+
+        #ifdef DEBUG
+            printf("Thread %d squaring position %d: ", args->thr_n, pos);
+            printf("%d**2 = ", (args->vector)[pos]);
+        #endif
+
+        (args->vector)[pos] *= (args->vector)[pos];
+
+        #ifdef DEBUG
+            printf("%d\n", (args->vector)[pos]);
+        #endif
+        
+    }
+
+    pthread_exit(NULL);
+}
