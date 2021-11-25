4482310e832f6ba2b77a9c67f8cd8420cd557ae6
Duda Ferreira <ferreira.mev@gmail.com>
2021-11-25 09:31:49 -0300

    Allocate and free memory

--- lab02/EduardaFerreira-CompConc-lab02-mat_mul.c
+++ lab02/EduardaFerreira-CompConc-lab02-mat_mul.c
@@ -8,7 +8,36 @@ Laboratório 2 -- Atividade 1
 #include <pthread.h>
 #include "timer.h"
 
+void seq_mat_mul(int dim, int* mat1, int* mat2, int* out)
+/* Função para multiplicação sequencial de matrizes quadradas
+dim x dim.
+
+A matriz de saída deve ter sido previamente alocada e fornecida
+por referência, por meio do parâmetro out.
+*/
+{
+
+}
+
 int main(int argc, char* argv[])
 {
+   int dim = 10;  // temporário, para teste
+
+   // Preenchendo matrizes de uns, para teste:
+   int* mat1 = malloc(sizeof(int) * dim * dim);
+   int* mat2 = malloc(sizeof(int) * dim * dim);
+   int* out = malloc(sizeof(int) * dim * dim);
+
+   if (!(mat1 && mat2 && out))
+   {
+      printf("Falha na alocação");
+      return EXIT_FAILURE;
+   }
+
+   // Liberando a memória alocada:
+   free(mat1);
+   free(mat2);
+   free(out);
+
    return EXIT_SUCCESS;
 }
