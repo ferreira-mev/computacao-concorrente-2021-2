1130f3df25750898784b0d230e923edd2fe30921
Duda Ferreira <ferreira.mev@gmail.com>
2021-11-25 09:05:57 -0300

    Create file for lab 2, activity 1

---
+++ lab02/EduardaFerreira-CompConc-lab02-mat_mul.c
@@ -0,0 +1,14 @@
+/* Computação Concorrente -- profa. Silvana Rossetto -- 2021.2 -- UFRJ
+Aluna: Eduarda Ferreira
+Laboratório 2 -- Atividade 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "timer.h"
+
+int main(int argc, char* argv[])
+{
+   return EXIT_SUCCESS;
+}
